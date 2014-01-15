5fe884e07b1ab9ec196ae43af36c74f8a1db61f4
Eoin Mc <eoinmc@gmail.com>
2014-01-15 22:47:51 +0000

    Started creating variables for scheduled task page

--- assets_create_task.php
+++ assets_create_task.php
@@ -1,7 +1,8 @@
 <?php
 	
 	/**
-	* Create a scheduled task that executes assets_scheduled_task.php each week.
+	* Create a scheduled task that executes assets_scheduled_task.php depending on user input
+	* Inserts details of created task to automate.scheduled_tasks
 	*/
 	$pagetitle = "Create Scheduled Task";
 	include("layout/header.php");
@@ -15,12 +16,21 @@
 			$invoice = "";
 	}
 	
-	$task_name = $invoice . " Lease Return - " . date('d.m.Y');
+	#Variables
+	$name 		= $invoice . " Lease Return - " . date('d.m.Y');
+	$schedule 	= "weekly";
+	$day		= "MON";
+	$time		= "08:00";
+	$start_date = "20/01/2014";
+	$end_date 	= "31/01/2014";
+	$user		= "NT AUTHORITY\SYSTEM";
+	
+	#Anything to be changed in the scheduled task query: assets_scheduled_task.php
+	$region 	= "";
 	
 	#When allowing user-supplied data to be passed to this function, use escapeshellarg() or escapeshellcmd() to ensure that users cannot trick the system into executing arbitrary commands.
-	#$command = "cmd /c schtasks /create /tn \"$task_name\" /tr \"C:\\xampp\\php\\php.exe -f C:\\xampp\\htdocs\\automate\\assets_scheduled_task.php $invoice\" /sc weekly /d MON /st 08:00 /ed 31/01/2014 /ru SYSTEM";
-	$command = "cmd /c schtasks /create /tn \"$task_name\" /tr \"C:\\xampp\\php\\php.exe -f C:\\xampp\\htdocs\\automate\\assets_scheduled_task.php $invoice\" /sc weekly /d MON /st 08:00 /ed 31/01/2014";
-	#echo $command;
+	$command = "cmd /c schtasks /create /tn \"$name\" /tr \"C:\\xampp\\php\\php.exe -f C:\\xampp\\htdocs\\automate\\assets_scheduled_task.php $invoice\" /sc $schedule /d $day /st $time /sd $start_date /ed $end_date";
+	
 	echo exec($command);
 	
 	include("layout/footer.php");
