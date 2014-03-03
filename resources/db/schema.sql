1e34fb9fdb0bb392993eb19713fdad5d71173c94
eoineoo <eoinmc@gmail.com>
2014-03-03 20:39:46 +0000

    Added automate.config

--- resources/db/schema.sql
+++ resources/db/schema.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2014 at 08:01 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
+-- Generation Time: Mar 03, 2014 at 09:39 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `automate`
 --
-CREATE DATABASE IF NOT EXISTS `automate` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `automate`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `config`
+--
+
+CREATE TABLE IF NOT EXISTS `config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `end_user` varchar(255) NOT NULL,
+  `machine_name` varchar(255) NOT NULL,
+  `applications` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=32 ;
+
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`id`, `user`, `end_user`, `machine_name`, `applications`, `timestamp`) VALUES
+(25, 'emccrann', 'IE\\emccrann', 'IECNU350C6YJ', 'dtlite4481-0347.msi', '2014-03-03 20:02:55'),
+(26, 'emccrann', 'IE\\emccrann', 'IECNU350C6YJ', 'GoogleChromeStandaloneEnterprise.msi, RDCMan.msi', '2014-03-03 20:10:07'),
+(27, 'emccrann', 'IE\\emccrann', 'IECNU350C6YJ', 'GoogleChromeStandaloneEnterprise.msi, ConfigMgrTools.msi, dtlite4481-0347.msi, FirefoxESR-24.2.0-en-GB.msi, gimp-2.2.8+gtk-2.6.8-1-en_US.msi, RDCMan.msi', '2014-03-03 20:13:48'),
+(28, 'emccrann', 'IE\\emccrann', 'IECNU350C6YJ', 'GoogleChromeStandaloneEnterprise.msi\n ConfigMgrTools.msi\n dtlite4481-0347.msi\n FirefoxESR-24.2.0-en-GB.msi\n gimp-2.2.8+gtk-2.6.8-1-en_US.msi\n RDCMan.msi', '2014-03-03 20:27:52'),
+(29, 'emccrann', 'IE\\emccrann', 'IECNU350C6YJ', 'GoogleChromeStandaloneEnterprise.msi\\n ConfigMgrTools.msi\\n dtlite4481-0347.msi\\n FirefoxESR-24.2.0-en-GB.msi\\n gimp-2.2.8+gtk-2.6.8-1-en_US.msi\\n RDCMan.msi', '2014-03-03 20:32:01'),
+(30, 'emccrann', 'IE\\emccrann', 'IECNU350C6YJ', 'GoogleChromeStandaloneEnterprise.msi<br /> ConfigMgrTools.msi<br /> dtlite4481-0347.msi<br /> FirefoxESR-24.2.0-en-GB.msi<br /> gimp-2.2.8+gtk-2.6.8-1-en_US.msi<br /> RDCMan.msi', '2014-03-03 20:32:44'),
+(31, 'emccrann', 'IE\\emccrann', 'IECNU350C6YJ', 'GoogleChromeStandaloneEnterprise.msi<br />ConfigMgrTools.msi<br />dtlite4481-0347.msi<br />FirefoxESR-24.2.0-en-GB.msi<br />gimp-2.2.8+gtk-2.6.8-1-en_US.msi<br />RDCMan.msi', '2014-03-03 20:34:18');
 
 -- --------------------------------------------------------
 
@@ -276,10 +303,249 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`staff_id`, `username`, `password`, `isadmin`) VALUES
 (4, 'emccrann', '76a7274804170464a31c6070ba4ccb05ff429f13', 'Yes');
 --
+-- Database: `cdcol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cds`
+--
+
+CREATE TABLE IF NOT EXISTS `cds` (
+  `titel` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `interpret` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
+  `jahr` int(11) DEFAULT NULL,
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `cds`
+--
+
+INSERT INTO `cds` (`titel`, `interpret`, `jahr`, `id`) VALUES
+('Beauty', 'Ryuichi Sakamoto', 1990, 1),
+('Goodbye Country (Hello Nightclub)', 'Groove Armada', 2001, 4),
+('Glee', 'Bran Van 3000', 1997, 5);
+--
+-- Database: `phpmyadmin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_bookmark`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_bookmark` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dbase` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `label` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `query` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Bookmarks' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_column_info`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_column_info` (
+  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `column_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `comment` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `mimetype` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `transformation` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `transformation_options` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `db_name` (`db_name`,`table_name`,`column_name`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Column information for phpMyAdmin' AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `pma_column_info`
+--
+
+INSERT INTO `pma_column_info` (`id`, `db_name`, `table_name`, `column_name`, `comment`, `mimetype`, `transformation`, `transformation_options`) VALUES
+(1, 'automate', 'config', 'id', '', '', '_', ''),
+(2, 'automate', 'config', 'user', '', '', '_', ''),
+(3, 'automate', 'config', 'end_user', '', '', '_', ''),
+(4, 'automate', 'config', 'machine_name', '', '', '_', ''),
+(6, 'automate', 'config', 'applications', '', '', '_', ''),
+(7, 'automate', 'config', 'timestamp', '', '', '_', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_designer_coords`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_designer_coords` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL,
+  `v` tinyint(4) DEFAULT NULL,
+  `h` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`db_name`,`table_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table coordinates for Designer';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_history`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_history` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `timevalue` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `sqlquery` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`,`db`,`table`,`timevalue`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='SQL history for phpMyAdmin' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_pdf_pages`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_pdf_pages` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `page_nr` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `page_descr` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  PRIMARY KEY (`page_nr`),
+  KEY `db_name` (`db_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='PDF relation pages for phpMyAdmin' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_recent`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_recent` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `tables` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Recently accessed tables';
+
+--
+-- Dumping data for table `pma_recent`
+--
+
+INSERT INTO `pma_recent` (`username`, `tables`) VALUES
+('root', '[{"db":"automate","table":"config"},{"db":"automate","table":"contact"},{"db":"automate","table":"users"},{"db":"phpmyadmin","table":"pma_column_info"},{"db":"phpmyadmin","table":"pma_designer_coords"},{"db":"phpmyadmin","table":"pma_history"},{"db":"phpmyadmin","table":"pma_pdf_pages"},{"db":"phpmyadmin","table":"pma_recent"},{"db":"phpmyadmin","table":"pma_relation"},{"db":"cdcol","table":"cds"}]');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_relation`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_relation` (
+  `master_db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `master_table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `master_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_db` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_table` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `foreign_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`master_db`,`master_table`,`master_field`),
+  KEY `foreign_field` (`foreign_db`,`foreign_table`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Relation table';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_table_coords`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_coords` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `pdf_page_number` int(11) NOT NULL DEFAULT '0',
+  `x` float unsigned NOT NULL DEFAULT '0',
+  `y` float unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`db_name`,`table_name`,`pdf_page_number`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table coordinates for phpMyAdmin PDF output';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_table_info`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_info` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `display_field` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  PRIMARY KEY (`db_name`,`table_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Table information for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_table_uiprefs`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_table_uiprefs` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `prefs` text COLLATE utf8_bin NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`username`,`db_name`,`table_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Tables'' UI preferences';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_tracking`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_tracking` (
+  `db_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `table_name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `version` int(10) unsigned NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime NOT NULL,
+  `schema_snapshot` text COLLATE utf8_bin NOT NULL,
+  `schema_sql` text COLLATE utf8_bin,
+  `data_sql` longtext COLLATE utf8_bin,
+  `tracking` set('UPDATE','REPLACE','INSERT','DELETE','TRUNCATE','CREATE DATABASE','ALTER DATABASE','DROP DATABASE','CREATE TABLE','ALTER TABLE','RENAME TABLE','DROP TABLE','CREATE INDEX','DROP INDEX','CREATE VIEW','ALTER VIEW','DROP VIEW') COLLATE utf8_bin DEFAULT NULL,
+  `tracking_active` int(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`db_name`,`table_name`,`version`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT COMMENT='Database changes tracking for phpMyAdmin';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pma_userconfig`
+--
+
+CREATE TABLE IF NOT EXISTS `pma_userconfig` (
+  `username` varchar(64) COLLATE utf8_bin NOT NULL,
+  `timevalue` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `config_data` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='User preferences storage for phpMyAdmin';
+
+--
+-- Dumping data for table `pma_userconfig`
+--
+
+INSERT INTO `pma_userconfig` (`username`, `timevalue`, `config_data`) VALUES
+('root', '2014-02-27 18:09:44', '{"collation_connection":"utf8mb4_general_ci"}');
+--
 -- Database: `swdata`
 --
-CREATE DATABASE IF NOT EXISTS `swdata` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `swdata`;
 
 -- --------------------------------------------------------
 
@@ -556,6 +822,31 @@ INSERT INTO `userdb` (`id`, `fullname`, `email`, `jobdesc`, `department`) VALUES
 (28, 'Zeph Reilly', 'zeph.reilly@company.ie', 'Associate', 'Audit'),
 (29, 'Luke Cruz', 'luke.cruz@company.ie', 'Associate', 'Audit'),
 (30, 'Kenneth Wolf', 'kenneth.wolf@company.ie', 'Intern', 'Audit');
+--
+-- Database: `test`
+--
+--
+-- Database: `webauth`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_pwd`
+--
+
+CREATE TABLE IF NOT EXISTS `user_pwd` (
+  `name` char(30) COLLATE latin1_general_ci NOT NULL DEFAULT '',
+  `pass` char(32) COLLATE latin1_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Dumping data for table `user_pwd`
+--
+
+INSERT INTO `user_pwd` (`name`, `pass`) VALUES
+('xampp', 'wampp');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
